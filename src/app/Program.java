e18ac9ad06375917f4fd55db9e2edb6e341ae44f
Oygods <lucas.luca2ft@gmail.com>
2022-08-24 21:14:51 -0300

    triangulos criados

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,7 +1,26 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double xA, xB, xC, yA, yB, yC;
+
+        System.out.println("Enter the measures of triangle X:");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y:");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        sc.close();
 
     }
 }
